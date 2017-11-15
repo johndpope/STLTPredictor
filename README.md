86d8d8b8a771fdf369c8412456a06b900f7ab53b
Tyler Kelly <tkelly@Tylers-MacBook-Pro.local>
2017-11-15 18:16:04 -0500

    added some database tracking

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # STLTPredictor
 
-Will search twitter for the top 500 companies based on name or stock symbol, will then analyze tweet contents and determine sentiment value of the tweet.
+Will search twitter for the top K companies based on name or stock symbol, will then analyze tweet contents and determine sentiment value of the tweet.
 
 
 Dependencies:
@@ -9,5 +9,3 @@ Dependencies:
 Depending on on your system, you might need to change the limit on open file descriptors allowed.
 
 You can check with `ulimit -a`, it seems we might need at least ~600, and you can change this with `ulimit -n value`
-
-The biggest issue is the twitter rate limit
--- retrieveTwitterData.py
+++ retrieveTwitterData.py
@@ -1,6 +1,7 @@
 from twython import Twython, TwythonError, TwythonRateLimitError
-import sqlite3 as lite
+import sqlite3
 import time
+from datetime import datetime
 from textblob import TextBlob
 import re
 import data
@@ -33,8 +34,14 @@ class TwitterClient(object):
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
     def get_tweet_sentiment(self, tweet):
+        """
+        1) tokenizes tweet
+        2) removes stop words
+        3) does part of speech tagging, selecting significant words
+        4) pass tokens to sentiment classifier which labels tokens from -1 to 1
+           based on data trained from a Naive Bayes Classfier
+        """
         analysis = TextBlob(self.clean_tweet(tweet))
-        # set sentiment
         return analysis.sentiment.polarity
 
     def get_tweets(self, query, count = 50):
@@ -42,45 +49,93 @@ class TwitterClient(object):
         retry = 0
         while(retry < 5):
             try:
-                fetched_tweets = self.auth.search(q=query.name + " OR " + query.symbol + " -filter:retweets AND -filter:replies", count=count)
+                """
+                search for tweets or replies that countain the company name or
+                stock symbol get sentiment for a tweet, record the company and
+                add to array to be returned
+                """
+                fetched_tweets = self.auth.search(q=query.name + " OR " + query.symbol + " -filter:retweets", count=count)
                 fetched_tweets = fetched_tweets['statuses']
                 for tweet in fetched_tweets:
                     parsed_tweet = {}
                     parsed_tweet['company'] = query
-                    parsed_tweet['tweet'] = tweet['text']
-                    parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet['text'])
+                    parsed_tweet['tweet'] = tweet['text'].encode('utf8')
+                    parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet['text'].encode('utf8'))
                     tweets.append(parsed_tweet)
 
                 return tweets
             except TwythonRateLimitError as e:
+                """
+                if we hit the rate limit, get the UTC epoch formatted time when
+                the limit will be reset, and wait for that long retry this 5
+                times, there is sort of a race condition here, as all remaining
+                threads in the pool will compete for resources (querying twitter)
+                """
                 to_sleep = float(self.auth.get_lastfunction_header('x-rate-limit-reset')) - time.time()
                 print "need to sleep for {0} seconds".format(int(to_sleep))
                 retry += 1
                 time.sleep(to_sleep)
             except TwythonError as e:
+                print e
                 return []
+def update_database(companies):
+    conn = sqlite3.connect('./static/project_db.sqlite')
+    with conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS Company (symbol TEXT PRIMARY KEY UNIQUE, name TEXT UNIQUE, sector TEXT, overall_sentiment REAL default 0);")
+        cur.execute("CREATE TABLE IF NOT EXISTS Sentiment (sid INTEGER PRIMARY KEY UNIQUE, value REAL, date_added TEXT, company_symbol TEXT, FOREIGN KEY(company_symbol) REFERENCES Company(symbol));")
+
+    for c in companies:
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT overall_sentiment from Company WHERE symbol=?", (c.symbol,))
+            cur_sent = cur.fetchone()
+            if cur_sent is None:
+                cur.execute("INSERT OR REPLACE INTO Company(symbol, name, sector, overall_sentiment) VALUES (?, ?, ?, ?);", \
+                        (c.symbol, c.name, c.sector, c.sentiment))
+            else:
+                cur_sent = float(cur_sent[0])
+                # partial solution to not adding to sentiment if same
+                if cur_sent != c.sentiment:
+                    cur.execute("INSERT OR REPLACE INTO Company(symbol, name, sector, overall_sentiment) VALUES (?, ?, ?, ?);", \
+                            (c.symbol, c.name, c.sector, cur_sent + c.sentiment))
+
+
+            dt = datetime.utcnow().strftime('%Y%m%d')
+            cur.execute("INSERT INTO Sentiment(value, date_added, company_symbol) VALUES (?, ?, ?);", \
+                        (c.sentiment, str(dt), c.symbol))
 
 def main():
+    # create array of Company objects from selected csv file
     companies = []
-    with open('./static/companyList.csv', 'r') as f:
+    with open('./static/known.csv', 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             companies.append(Company(row[0], row[1], row[2]))
-    # create N threads in a pool
-    # each thread will query twitter with the companien info
-    # store in results
+    """
+    create N threads in a pool
+    each thread will query twitter with the company info
+    store in results
+    """
     pool = ThreadPool(len(companies))
     api = TwitterClient()
     results = pool.map(api.get_tweets, companies)
 
     pool.close()
     pool.join()
+
     for re in results:
         for r in re:
-            #increment sentiment of company
-            companies[next(index for (index, d) in enumerate(companies) if d.name == r['company'].name)].set_sentiment(float(r['sentiment']))
+            #increment sentiment of selected company
+            try:
+                companies[next(index for (index, d) in enumerate(companies) if d.name == r['company'].name)].set_sentiment(float(r['sentiment']))
+            except:
+                continue
+
     for c in companies:
         print c.name + "|" + str(c.sentiment)
+
+    update_database(companies)
 
 if __name__ == "__main__":
     main()
