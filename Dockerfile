9a7c267e5b5c174ec89e7a0d89c1d1dc3e5251e4
Tyler Kelly <tkelly@Tylers-MacBook-Pro.local>
2017-12-09 17:41:50 -0500

    worker works with knn

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,7 @@ RUN add-apt-repository -y ppa:mc3man/trusty-media
 RUN apt-get -y install ffmpeg
 RUN apt-get -y install libxml2-dev libxslt-dev python-dev lib32z1-dev
 RUN apt-get -y install python-pip
+RUN apt-get -y install python-tk
 
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
 
--- app/knnPredictor/knnPredictor.py
+++ app/knnPredictor/knnPredictor.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 
 
 def KNN(sentiment, emotion):
-    
+
     # name os csv file with train info (results.csv)
-    df = pd.read_csv('results.csv')
-    
+    #df = pd.read_csv('results.csv')
+
     # alternative for docker
-    #df = pd.read_csv('/app/results.csv')
-    
+    df = pd.read_csv('/app/results.csv')
+
     df.replace('?',-99999, inplace=True)
 
     # drop any unwanted fields
@@ -28,12 +28,12 @@ def KNN(sentiment, emotion):
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
     # use k = 4 neighbors when predicting
-    clf = neighbors.KNeighborsClassifier(4)     
+    clf = neighbors.KNeighborsClassifier(4)
 
     clf.fit(X_train, y_train)
 
     accuracy = clf.score(X_test, y_test)
-    print"Testing Accuracy:", accuracy
+    #print"Testing Accuracy:", accuracy
 
     # this is what we want to predict on
     example_measures = np.array([sentiment, emotion])
@@ -41,45 +41,45 @@ def KNN(sentiment, emotion):
 
     prediction = clf.predict(example_measures)
     prediction_proba = clf.predict_proba(example_measures)
-    
+    """
     print "Bad Buy Probability:", prediction_proba.tolist()[0][0]
     print "Good Buy Probability:", prediction_proba.tolist()[0][1]
     print ""
-    
+
     #print "Prediction: ", prediction_proba.tolist()[0][prediction[0]]
-    
+
     if prediction.tolist()[0] == 0:
         print "Prediction: Bad Buy"
     else:
         print "Prediction: Good Buy"
-    
+
     #print(prediction[0])
-    
+
     k_list = []     # build the good_buy class(k = black points)
     r_list = []     # build the bad_buy class (r= red points)
-    
+
     point_i = 0
     for point in x_list:
-        if y_list[point_i] == 1:        # check if weight is 1 
+        if y_list[point_i] == 1:        # check if weight is 1
             k_list.append(point)
         else:
             r_list.append(point)
-            
+
         point_i += 1
-            
+
     # add new point to graph
-    new_features = [sentiment, emotion]     
-            
+    new_features = [sentiment, emotion]
+
     # k and r will also be used for the color (black, red)
-    dataset = { 'k': k_list, 'r': r_list }      
+    dataset = { 'k': k_list, 'r': r_list }
 
     #print "dataset:", dataset
-    
+
     black_patch = mpatches.Patch(color='black', label='Good Buy')
     red_patch = mpatches.Patch(color='red', label='Bad Buy')
-    
+
     plt.legend(handles=[black_patch, red_patch])
-    
+
     [[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]] for i in dataset]
     plt.scatter(new_features[0], new_features[1], s=100)
 
@@ -88,14 +88,12 @@ def KNN(sentiment, emotion):
     plt.ylabel('Emotion')
 
     plt.show()
-    
+    """
     # returns 0 if bad buy, 1 if good buy
     return prediction[0].tolist(), prediction_proba.tolist()[0][prediction[0]]
 
 if __name__ == "__main__":
     KNN(0.025, 0.0);
-    
-    
+
+
 # some code obtained and adapted from https://pythonprogramming.net/programming-k-nearest-neighbors-machine-learning-tutorial
-
-
