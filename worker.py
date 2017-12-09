40c1cd38b63524aaba39c448f0d6ab4e110adfb7
Tyler Kelly <tkelly@Tylers-MacBook-Pro.local>
2017-12-09 17:41:50 -0500

    worker works with knn

--- worker.py
+++ worker.py
@@ -1,16 +1,23 @@
 import sys
+import os
 sys.path.append("/app/app/emotion_tensorflow/")
 import predict as emotionalRecognizer
 sys.path.append("/app/app/twitterExtraction/")
 import retrieveTwitterData as sentimentScorer
-from ffmpy import FFmpeg
-import os
+sys.path.append("/app/app/knnPredictor/")
+
 def blockPrint():
     sys.stdout = open(os.devnull, 'w')
+    sys.stderr = open(os.devnull, 'w')
+blockPrint()
+
+import knnPredictor as Predictor
+from ffmpy import FFmpeg
 
 # Restore
 def enablePrint():
     sys.stdout = sys.__stdout__
+    sys.stderr = sys.__stderr__
 
 def extractVideo(video_f, directory):
     ff = FFmpeg(
@@ -23,8 +30,9 @@ def extractVideo(video_f, directory):
     enablePrint()
 
 if __name__ =="__main__":
+    enablePrint()
     if len(sys.argv) < 2:
-        print "usage: python worker.py SYS_YYYY-MM-DD.mp4"
+        print "usage: python worker.py SYM_YYYY-MM-DD.mp4"
         sys.exit()
     video_f = sys.argv[1]
     company_sym = video_f.partition("_")[0]
@@ -33,13 +41,15 @@ if __name__ =="__main__":
     try:
         os.makedirs(directory)
     except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
+        print "{0} already exists".format(directory)
+        raise
     extractVideo(video_f, directory + "/" + company_sym + "_image_sequence%06d.png")
 
     print "determing sentiment score..."
     sentiment_score = sentimentScorer.main(company_sym, date)
     print "sentiment score: {0}".format(sentiment_score)
     print "determining emotional score..."
-    emotional_score = emotionalRecognizer.predict('companies/MCD', 'extract')
+    emotional_score = emotionalRecognizer.predict("companies/" + video_f[:-4])
     print "emotional score: {0}".format(emotional_score)
+    prediction, probability = Predictor.KNN(sentiment_score, emotional_score)
+    print "you should {0} with probability {1}".format("BUY" if prediction == 1 else "NOT BUY", probability)
